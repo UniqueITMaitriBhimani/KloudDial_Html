e0c86da42f76ec639e139ee8153dd2d3a16e613b
UniqueITMaitriBhimani <maitri.bhimani@theuniqueitsolution.com>
2024-08-30 10:39:03 +0530

    Changes in Help Desk and other

--- assets/js/dev-script.js
+++ assets/js/dev-script.js
@@ -1,13 +1,37 @@
-function changeColor(card, activeClass) {
-    
+function changeColor(card, activeClass, tableId) {
+  
     document.querySelectorAll('.status-card').forEach(function(el) {
         el.classList.remove('pending-active', 'working-active', 'completed-active');
     });
 
+    
+    document.querySelectorAll('.status-table').forEach(function(table) {
+        table.parentElement.classList.add('d-none');
+    });
 
     card.classList.add(activeClass);
+
+   
+    document.getElementById(tableId).classList.remove('d-none');
 }
 
-function showTable() {
-  
-}
+  document.getElementById('reject').addEventListener('click', function() {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, reject it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+     
+        Swal.fire(
+          'Rejected!',
+          'Your request has been rejected.',
+          'success'
+        );
+      }
+    });
+  });
